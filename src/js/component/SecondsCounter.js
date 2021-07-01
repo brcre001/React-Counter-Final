3a5953fd43fa542547618f5ece198e06aa75d599
brcre001 <brandon.f.creed@gmail.com>
2021-07-01 20:24:30 +0000

    first commit of second simple counter exercise

---
+++ src/js/component/SecondsCounter.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const SecondsCounter = props => {
+	return (
+		<div className="container">
+			<div className="box">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="box">{props.digitSix % 10}</div>
+			<div className="box">{props.digitFive % 10}</div>
+			<div className="box">{props.digitFour % 10}</div>
+			<div className="box">{props.digitThree % 10}</div>
+			<div className="box">{props.digitTwo % 10}</div>
+			<div className="box">{props.digitOne % 10}</div>
+		</div>
+	);
+};
+
+SecondsCounter.propTypes = {
+	digitOne: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitFour: PropTypes.number,
+	digitFive: PropTypes.number,
+	digitSix: PropTypes.number
+};
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,31 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { SecondsCounter } from "./component/SecondsCounter.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let counter = 0;
+
+setInterval(() => {
+	counter++;
+	let one = Math.floor(counter / 1);
+	let two = Math.floor(counter / 10);
+	let three = Math.floor(counter / 100);
+	let four = Math.floor(counter / 1000);
+	let five = Math.floor(counter / 10000);
+	let six = Math.floor(counter / 100000);
+	//render your react application inside the set interval
+
+	ReactDOM.render(
+		<SecondsCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+			digitFive={five}
+			digitSix={six}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);

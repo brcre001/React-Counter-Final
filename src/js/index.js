88d081de02d73695ec8b0847e43b14147ba6cb6d
brcre001 <brandon.f.creed@gmail.com>
2021-07-01 21:25:20 +0000

    added input with button

--- src/js/index.js
+++ src/js/index.js
@@ -23,17 +23,40 @@ setInterval(() => {
 	let four = Math.floor(counter / 1000);
 	let five = Math.floor(counter / 10000);
 	let six = Math.floor(counter / 100000);
-	//render your react application inside the set interval
 
+	//render your react application inside the set interval
 	ReactDOM.render(
-		<App
-			placeOne={one}
-			placeTwo={two}
-			placeThree={three}
-			placeFour={four}
-			placeFive={five}
-			placeSix={six}
-		/>,
+		<>
+			<App
+				placeOne={one}
+				placeTwo={two}
+				placeThree={three}
+				placeFour={four}
+				placeFive={five}
+				placeSix={six}
+			/>
+			<div className="container">
+				<form>
+					<div className="input-group mb-3">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Enter a number"
+							aria-label="Input Number"
+							aria-describedby="button-addon2"
+						/>
+						<div className="input-group-append">
+							<button
+								className="btn btn-success"
+								type="submit"
+								id="button-addon2">
+								Submit
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</>,
 		document.querySelector("#app")
 	);
 }, 1000);

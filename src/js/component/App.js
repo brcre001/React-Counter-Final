0b9023c5859667eede07b2a735589d35c4eae067
brcre001 <brandon.f.creed@gmail.com>
2021-07-04 20:06:12 +0000

    added useState and useEffect to change timer values

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -1,25 +1,34 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
 import { SecondsCounter } from "./SecondsCounter.js";
 
-export const App = props => {
+export const App = () => {
+	// Using setState for counter variable
+	const [count, setCount] = useState(0);
+
+	// Using useEffect to increase value after each time it is loaded
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCount(prevCount => prevCount + 1);
+		}, 1000);
+		return () => clearInterval(interval);
+	}, []);
+
+	// Setting variables for each position on the timer
+	let one = Math.floor(count / 1);
+	let two = Math.floor(count / 10);
+	let three = Math.floor(count / 100);
+	let four = Math.floor(count / 1000);
+	let five = Math.floor(count / 10000);
+	let six = Math.floor(count / 100000);
+
 	return (
 		<SecondsCounter
-			digitOne={props.placeOne}
-			digitTwo={props.placeTwo}
-			digitThree={props.placeThree}
-			digitFour={props.placeFour}
-			digitFive={props.placeFive}
-			digitSix={props.placeSix}
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+			digitFive={five}
+			digitSix={six}
 		/>
 	);
-};
-
-App.propTypes = {
-	placeOne: PropTypes.number,
-	placeTwo: PropTypes.number,
-	placeThree: PropTypes.number,
-	placeFour: PropTypes.number,
-	placeFive: PropTypes.number,
-	placeSix: PropTypes.number
 };
